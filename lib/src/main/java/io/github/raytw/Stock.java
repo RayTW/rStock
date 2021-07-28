d13845b680cc89fa9b43ab97e8c67c3adc7e89f0
Ray <ray00000sina@gmail.com>
2021-07-28 22:33:53 +0800

    init.

---
+++ lib/src/main/java/io/github/raytw/Stock.java
@@ -0,0 +1,12 @@
+package io.github.raytw;
+
+/**
+ * main.
+ *
+ * @author Ray Li
+ */
+public class Stock {
+  public boolean someLibraryMethod() {
+    return true;
+  }
+}
---
+++ lib/src/test/java/io/github/raytw/StockTest.java
@@ -0,0 +1,13 @@
+package io.github.raytw;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+  @Test
+  void someLibraryMethodReturnsTrue() {
+    Stock classUnderTest = new Stock();
+    assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+  }
+}
